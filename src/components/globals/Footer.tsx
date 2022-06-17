f7eb6c566c4a3583d96bca1a8ca0dbba1de54826
JohnnyHiriart <johnnyhc@hotmail.com>
2022-06-17 16:14:41 +0200

    footer code clean

--- src/components/globals/Footer.tsx
+++ src/components/globals/Footer.tsx
@@ -10,32 +10,48 @@ const Footer = () => {
         <div className="container__footer">
           <ul>
             <li>
-              <a target="_blank" href="https://www.google.fr/">Nous contacter</a>
+              <a target="_blank" href="https://www.google.fr/" rel="noreferrer">
+                Nous contacter
+              </a>
             </li>
             <li>
-              <a target="_blank" href="https://www.google.fr/">Mentions légales</a>
+              <a target="_blank" href="https://www.google.fr/" rel="noreferrer">
+                Mentions légales
+              </a>
             </li>
             <li>
-              <a target="_blank" href="https://www.google.fr/">Conditions générales de ventes </a>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a target="_blank" href="https://www.google.fr/">Retours et échanges</a>
-            </li>
-            <li>
-              <a target="_blank" href="https://www.google.fr/">Informations de livraisons</a>
+              <a target="_blank" href="https://www.google.fr/" rel="noreferrer">
+                Conditions générales de ventes{' '}
+              </a>
             </li>
           </ul>
           <ul>
             <li>
-              <a target="_blank" href="https://www.google.fr/">Twitter</a>
+              <a target="_blank" href="https://www.google.fr/" rel="noreferrer">
+                Retours et échanges
+              </a>
             </li>
             <li>
-              <a target="_blank" href="https://www.google.fr/">Instagram</a>
+              <a target="_blank" href="https://www.google.fr/" rel="noreferrer">
+                Informations de livraisons
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a target="_blank" href="https://www.google.fr/" rel="noreferrer">
+                Twitter
+              </a>
             </li>
             <li>
-              <a target="_blank" href="https://www.google.fr/">Facebook</a>
+              <a target="_blank" href="https://www.google.fr/" rel="noreferrer">
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://www.google.fr/" rel="noreferrer">
+                Facebook
+              </a>
             </li>
           </ul>
         </div>
