8b50bc73bf36217aaf2ba93354c86cdd692c61e1
Nawel Benaicha <nawelbenaicha@MacBook-Pro-de-Nawel.local>
2022-06-17 15:41:16 +0200

    HomePage review

--- src/components/homepage/EssentialsPage.tsx
+++ src/components/homepage/EssentialsPage.tsx
@@ -9,19 +9,15 @@ const EssentialsPage = () => {
       </div>
       <div className="essentialsPage__container">
         <div className="essentialsPage__firstImage">
-          <img id="firstbag" src="../assets/images/sac1.png" alt="sac sur une futa" />
+          <img id="firstbag" src="../assets/images/sac1.png" alt="bag big size" />
         </div>
         <div className="essentialsPage__secondImage">
           {' '}
-          <img
-            id="secondbag"
-            src="../assets/images/sac2.png"
-            alt="sac et mannequin"
-          />{' '}
+          <img id="secondbag" src="../assets/images/sac2.png" alt="bag and model" />{' '}
         </div>
         <div className="essentialsPage__thirdImage">
           {' '}
-          <img id="thirdbag" src="../assets/images/sac3.png" alt="sac sur table" />{' '}
+          <img id="thirdbag" src="../assets/images/sac3.png" alt="bag middle size" />{' '}
         </div>
       </div>
     </div>
