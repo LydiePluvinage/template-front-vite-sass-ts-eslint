2959544378fb104b634146dfd3ee68f67ddcfdb9
JacquesPoulin <jacques.poulin64@gmail.com>
2022-06-17 15:02:08 +0200

    [FEATURES] : Navbar routes done

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import Footer from '../src/components/globals/Footer';
 import Navbar from '../src/components/globals/Navbar';
 import Collection from './pages/Collection';
 import Homepage from './pages/Homepage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Account from './pages/Account';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -17,6 +21,10 @@ function App() {
           <Route path="*" element={<Homepage />} />
           <Route path="/home" element={<Homepage />} />
           <Route path="/collection" element={<Collection />} />
+          <Route path="/univers" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/compte" element={<Account />} />
+          <Route path="/panier" element={<Cart />} />
         </Routes>
         <Footer />
       </HashRouter>
