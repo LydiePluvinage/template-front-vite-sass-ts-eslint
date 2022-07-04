237877d4716c81be2af933bad1e666bf2cb32f26
JacquesPoulin <jacques.poulin64@gmail.com>
2022-07-04 15:24:04 +0200

    [FEATURES] : Find Component 100% Done

--- src/components/globals/Find.tsx
+++ src/components/globals/Find.tsx
@@ -1,21 +1,26 @@
-import Button from '@mui/material/Button';
-import axios from 'axios';
-import SearchBar from 'material-ui-search-bar';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// ---- @mui components -----
+import Button from '@mui/material/Button';
+import SearchBar from 'material-ui-search-bar';
+
+// ---- react-animated-css package  -----
 import { Animated } from 'react-animated-css';
 
+// ---- Imported components -----
 import IProduct from '../../interfaces/IProduct';
 import GoToTop from '../globals/GoToTop';
 
+// -------------------- COMPONENT ---------------------------------
 const Find = () => {
   // ---- States -----
   const [allProducts, setAllProducts] = useState<IProduct[]>();
-  const [userChoice, setUserChoice] = useState('');
+  const [userChoice, setUserChoice] = useState<string>('');
 
-  // ---- Func -----
-
-  const findProduct = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserChoice(e.target.value);
+  // ---- Function to handle the button click -----
+  const showCollection = () => {
+    setUserChoice('');
   };
 
   // >> Axios to get all Products
@@ -37,46 +42,57 @@ const Find = () => {
       {/* --- SEARCH BAR --- */}
       <div className="searchContainer">
         <div className="searchContainer__inputContainer">
-          <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
+          <Animated animationIn="fadeInLeft" animationOut="fadeOut" isVisible={true}>
             <SearchBar
               className="searchContainer__inputContainer__input"
               value={userChoice}
               placeholder="Recherche"
-              onChange={findProduct}
+              onChange={(newValue) => setUserChoice(newValue)}
             />
           </Animated>
+          {console.log(userChoice)}
         </div>
 
-        {/* --- PRODUCT ITEMS MAPPING --- */}
+        {/* --- PRODUCT ITEMS FILTER & MAPPING --- */}
         <div className="searchContainer__displayAll">
-          <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
-            <Button variant="text" className="searchContainer__displayAll__button">
-              TOUTE LA COLLECTION
-            </Button>
+          <Animated animationIn="fadeInRight" animationOut="fadeOut" isVisible={true}>
+            {userChoice !== '' && (
+              <Button
+                variant="text"
+                className="searchContainer__displayAll__button"
+                onClick={showCollection}>
+                VOIR TOUTE LA COLLECTION
+              </Button>
+            )}
           </Animated>
         </div>
         <div className="searchContainer__collectionWrapper">
           {allProducts &&
-            allProducts.map(({ id, productImage, productName }) => (
-              <Animated
-                key={id}
-                animationIn="fadeIn"
-                animationOut="fadeOut"
-                isVisible={true}>
-                <div className="searchContainer__collectionWrapper__container">
-                  <img
-                    id="collectionBags"
-                    src={productImage}
-                    alt="Sacs de la marque Brille"
-                    className="searchContainer__collectionWrapper__container__image"
-                  />
-                  <p className="searchContainer__collectionWrapper__container__text">
-                    {' '}
-                    {productName}
-                  </p>
-                </div>
-              </Animated>
-            ))}
+            allProducts
+              .filter(
+                ({ productName }) =>
+                  productName.toLowerCase().includes(userChoice.toLowerCase()) ||
+                  !userChoice,
+              )
+              .map(({ id, productImage, productName }) => (
+                <Animated
+                  key={id}
+                  animationIn="fadeIn"
+                  animationOut="fadeOut"
+                  isVisible={true}>
+                  <div className="searchContainer__collectionWrapper__container">
+                    <img
+                      id="collectionBags"
+                      src={productImage}
+                      alt="Sacs de la marque Brille"
+                      className="searchContainer__collectionWrapper__container__image"
+                    />
+                    <p className="searchContainer__collectionWrapper__container__text">
+                      {productName}
+                    </p>
+                  </div>
+                </Animated>
+              ))}
         </div>
       </div>
       <GoToTop />
